8799643b329f95df613ded7ccce161ec406b5f30
altan.ay204 <altan.ay204@gmail.com>
2025-12-06 09:59:53 +0000

    Specified path for build

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import plugin from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/chess-siege/',
     plugins: [plugin()],
     server: {
         port: 56383,
